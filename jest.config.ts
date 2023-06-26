0847cefaa78cf7e99cb92e409e062c766ef76283
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2023-06-26 10:14:01 +0200

    feat: :sparkles: init project with testing packages: jest, sinon and supertest

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import type { JestConfigWithTsJest } from "ts-jest";
+
+const config: JestConfigWithTsJest = {
+  preset: "ts-jest/presets/default-esm",
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  workerIdleMemoryLimit: "500M",
+};
+
+export default config;
