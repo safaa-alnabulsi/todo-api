bad508b5c47e27e6adf90d012b86b8e5b8eb78fb
Safaa Alnabulsi <safaa.alnablsi@gmail.com>
2023-07-05 17:02:11 +0200

    feat: add more tests to cover other endpoints

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,10 +1,12 @@
 import request from "supertest";
-import sinon from "sinon";
+import sinon, { SinonStub } from "sinon";
 import Application from "../dist/application.js";
 import router from "../dist/routes/index.js";
 import DbWrapper from "../dist/database/dbWrapper.js";
 import mongoose from "mongoose";
 import Todo from "../dist/models/todo.js";
+import ITodo from "../dist/types/todo.js";
+import { json } from "body-parser";
 
 let application: Application;
 let app: Express.Application;
@@ -18,7 +20,7 @@ const todos = [
     _id: "6495ad459747087bbe24729c",
     name: "todo1",
     description: "my next todo1",
-    status: true,
+    status: false,
     createdAt: "2023-06-23T14:33:41.890Z",
     updatedAt: "2023-06-23T14:55:21.677Z",
     __v: 0,
@@ -104,6 +106,141 @@ describe("Test GET one TODO /todos/:id Endpoint ", () => {
     expect(response.status).toEqual(200);
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.body).toEqual({ todo: todos[0] });
+
+    TodoMock.verify();
+    TodoMock.restore();
+  });
+});
+
+describe("Test Create POST /todos/:id Endpoint ", () => {
+  let saveStub: SinonStub;
+
+  beforeEach(() => {
+    saveStub = sinon.stub(Todo.prototype, "save");
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("should send 201 and create one new todo", async () => {
+    const savedTodo = {
+      _id: "test-id",
+      name: "Test Todo3",
+      description: "Test description 3",
+      status: false,
+    };
+
+    saveStub.resolves(savedTodo);
+
+    const response = await request(app).post("/todos").send({
+      name: "Test Todo3",
+      description: "Test description 3",
+      status: false,
+    });
+
+    sinon.assert.calledOnce(saveStub);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toEqual({
+      message: "Todo item has been added successfully",
+      todo: savedTodo,
+    });
+  });
+});
+
+describe("Test Update PUT /todos/:id Endpoint", () => {
+  it("should send 202 and update already existing todo", async () => {
+    const id = "6495ad459747087bbe24729c";
+    const body = {
+      name: "Updated Todo",
+      description: "Updated description",
+      status: true,
+    };
+    const updatedTodo = {
+      _id: id,
+      ...body,
+    };
+
+    const TodoMock = sinon.mock(Todo);
+    TodoMock.expects("findByIdAndUpdate")
+      .withArgs({ _id: id }, body)
+      .resolves(updatedTodo);
+
+    const response = await request(app).put(`/todos/${id}`).send({
+      name: "Updated Todo",
+      description: "Updated description",
+      status: true,
+    });
+
+    expect(response.status).toEqual(202);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.body).toEqual({
+      message: "Todo item has been updated successfully",
+      todo: updatedTodo,
+    });
+
+    TodoMock.verify();
+    TodoMock.restore();
+  });
+});
+
+describe("Test PATCH /todos/:id Endpoint", () => {
+  it("should send 202 and mark the todo with true", async () => {
+    const id = "6495ad459747087bbe24729c";
+    const body = {
+      name: "Pacthed Todo",
+      description: "Pacthed description",
+      status: true,
+    };
+
+    const patchedTodo = {
+      _id: id,
+      ...body,
+    };
+
+    const TodoMock = sinon.mock(Todo);
+    TodoMock.expects("findByIdAndUpdate")
+      .withArgs(
+        { _id: id },
+        {
+          status: "true",
+        }
+      )
+      .resolves(patchedTodo);
+
+    const response = await request(app).patch(`/todos/${id}`);
+
+    expect(response.status).toEqual(202);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.body).toEqual({
+      message: "Todo item has been marked as completed successfully",
+      todo: patchedTodo,
+    });
+
+    TodoMock.verify();
+    TodoMock.restore();
+  });
+});
+
+describe("Test DELETE /todos/:id Endpoint", () => {
+  it("should send 202 and delete already existing todo", async () => {
+    const id = "6495ad459747087bbe24729c";
+
+    const TodoMock = sinon.mock(Todo);
+
+    TodoMock.expects("findByIdAndDelete")
+      .withArgs({ _id: id })
+      .resolves(todos[0]);
+
+    const response = await request(app).delete(`/todos/${id}`);
+
+    expect(response.status).toEqual(202);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.body).toEqual({
+      message: "Todo item has been deleted successfully",
+      todo: todos[0],
+    });
 
     TodoMock.verify();
     TodoMock.restore();
